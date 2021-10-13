77f4d122cffbc96209e919b705a2532b4f0768ac
Shashidhar Kumar Paladi <shashidharpaladi@Shashidhars-MacBook-Pro.local>
2021-10-13 16:32:20 +0530

    added/updated and deleted code

--- src/main/java/PracticeSelenium/AmazonTest.java
+++ src/main/java/PracticeSelenium/AmazonTest.java
@@ -7,6 +7,7 @@ public class AmazonTest {
 		BrowserUtil br = new BrowserUtil();
 		br.initDriver("chrome");
 		br.launchUrl("http://www.amazon.com");
+		System.out.println("url is launched....");
 		String title = br.getPageTitle();
 		System.out.println("page title is: "+title);
 		System.out.println(br.getPageUrl());
---
+++ src/main/java/PracticeSelenium/TestDemo.java
@@ -0,0 +1,11 @@
+package PracticeSelenium;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("testing Demo");
+
+	}
+
+}
